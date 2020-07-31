edc7fab7f221ba21b5202ffbf9c70f99215b27eb
soohyeon.lee <soohyeon.lee@kakao.com>
2020-08-01 01:05:13 +0900

    메인화면, 모임 조회 구현 #15

--- ios/ios/View_login.swift
+++ ios/ios/View_login.swift
@@ -8,105 +8,187 @@
 
 import SwiftUI
 
-struct View_login: View {
+struct ViewLogin: View {
     
-	@State var textfield_ID:String = ""
-	@State var textfield_password:String = ""
-	
-	var body: some View {
-		NavigationView {
-			VStack(alignment: .center, spacing: 45) {
-				VStack() {
-					Text("모각코").font(.system(size: 50))
-					Text("CodingTogether")
-				}
-				
-				Image("login_image")
-					.resizable()
-					.aspectRatio(contentMode: .fit)
-					.cornerRadius(75)
-				
-				HStack(spacing: 20) {
-					VStack(alignment: .center, spacing: 30) {
-						Text("ID").font(.system(size: 25))
-						Text("Password").font(.system(size: 25))
-					}
-					VStack(alignment: .center, spacing: 30) {
-						TextField("Input your ID", text:$textfield_ID)
-							.font(.system(size: 25))
-						SecureField("Input your password", text:$textfield_password)
-							.font(.system(size: 25))
-					}
-				}
-				VStack(alignment: .center, spacing: 10) {
-					Button(action: click_button_login, label: {
-						HStack {
-							Text("LOGIN")
-								.fontWeight(.semibold)
-								.font(.title)
-								.padding(10)
-						}
-					})
-					.frame(minWidth:0, maxWidth:.infinity)
-					.foregroundColor(.white)
-					.background(Color.green)
-					.cornerRadius(40)
-					
-					NavigationLink(destination: View_join(), label: {
-						Text("JOIN")
-						.fontWeight(.semibold)
-						.font(.title)
-						.padding(10)
-						.frame(minWidth:0, maxWidth:.infinity)
-						.foregroundColor(.white)
-						.background(Color.green)
-						.cornerRadius(40)
-						})
-				}
-			}.padding([.leading, .bottom, .trailing], 20)
-		}
+    @EnvironmentObject var myInfo: MyInfo
+    
+    @State var textFieldID: String = ""
+    @State var textFieldPassword: String = ""
+    
+    @State var isSuccessLogin: Bool = false
+    @State var isJoin: Bool = false
+    
+    @State var showAlertLogin: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            
+            VStack {
+                
+                VStack() {
+                    Text("모각코").font(.largeTitle)
+                    Text("CodingTogether").font(.subheadline)
+                }.padding(10)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment:.center)
+                
+                Spacer()
+                
+                Image("login_image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(75)
+                    .padding(10)
+                
+                Spacer()
+                
+                VStack(spacing: 20) {
+                    HStack(spacing: 20) {
+                        Text("ID")
+                            .font(.title)
+                            .frame(width: 100, alignment: .center)
+                        TextField("Input your ID", text:$textFieldID)
+                            .font(.title)
+                    }
+                    HStack(spacing: 20) {
+                        Text("Password")
+                            .font(.headline)
+                            .frame(width: 100, alignment: .center)
+                        SecureField("Input your password", text:$textFieldPassword)
+                            .font(.title)
+                    }
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, alignment:.center)
+                .padding(10)
+                
+                Spacer()
+                
+                VStack(spacing: 10) {
+                    
+                    NavigationLink(destination: ViewMain(codingTogetherList: CodingTogetherList())
+                        , isActive: self.$isSuccessLogin, label: {
+                            Button(action: {
+                                if self.textFieldID != "" && self.textFieldPassword != "" {
+                                    self.clickButtonForLogin()
+                                }
+                            }, label: {
+                                Text("LOGIN")
+                                    .fontWeight(.semibold)
+                                    .font(.title)
+                                    .padding(10)
+                            })
+                                .frame(minWidth:0, maxWidth:.infinity)
+                                .foregroundColor(.white)
+                                .background(Color.green)
+                                .cornerRadius(40)
+                                
+                                
+                                .actionSheet(isPresented: self.$showAlertLogin) {
+                                    
+                                    ActionSheet(title: Text("로그인 실패"), message: Text("ID 혹은 비밀번호가 일치하지 않습니다. \n 확인 후 다시 시도하세요"), buttons: [.default(Text("확인"))])
+                            }
+                    })
+                    
+                    NavigationLink(destination: ViewJoin(), isActive: self.$isJoin, label: {
+                        Button(action: {
+                            self.isJoin.toggle()
+                        }, label: {
+                            Text("JOIN")
+                                .fontWeight(.semibold)
+                                .font(.title)
+                                .padding(10)
+                        })
+                            .frame(minWidth:0, maxWidth:.infinity)
+                            .foregroundColor(.white)
+                            .background(Color.green)
+                            .cornerRadius(40)
+                    })
+                }.padding(10)
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+            .padding(10)
+            .navigationBarTitle("", displayMode: .inline)
+            .navigationBarBackButtonHidden(true)
+            
+        }
+        
     }
-	
-	func click_button_login() {
-		
-		let id = textfield_ID
-		let pw = textfield_password
-		
-		if id == "" || pw == "" {
-			return
-		}
-		
-		let url_for_request = URL(string: "http://139.150.64.36/auth/login")
-	
-		let pw_encoded = pw.data(using: .utf8)?.base64EncodedString()
-		
-		let raw: [String : Any] = ["user_id": id, "user_pw":pw_encoded!]
-		
-		let formDataString = (raw.flatMap({ (key, value) -> String in return "\(key)=\(value)" }) as Array).joined(separator: "&")
-
-		var request = URLRequest(url: url_for_request!)
-		request.httpMethod = "POST"
-		request.httpBody = formDataString.data(using: .utf8)
-		
-		let task = URLSession.shared.dataTask(with: request) {
-			(data, response, error) in
-			
-			if let e = error{
-				print("끄엑", e)
-			}
-			
-			DispatchQueue.main.async() {
-				let outputStr = String(data: data!, encoding: String.Encoding.utf8)
-				print("result: \(outputStr!)")
-			}
-		}
-		
-		task.resume()
-	}
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.shadowColor = .clear
+        appearance.configureWithTransparentBackground()
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
+    
+    func clickButtonForLogin() {
+        
+        let id = textFieldID
+        let pw = textFieldPassword
+        
+        let urlForRequest = URL(string: "http://139.150.64.36:9530/auth/login")
+        
+        let encodedPW = pw.data(using: .utf8)?.base64EncodedString()
+        
+        let raw: [String : Any] = ["userID": id, "userPW":encodedPW!]
+        
+        let formDataString = (raw.compactMap({ (key, value) -> String in return "\(key)=\(value)" }) as Array).joined(separator: "&")
+        
+        var request = URLRequest(url: urlForRequest!)
+        request.httpMethod = "POST"
+        request.httpBody = formDataString.data(using: .utf8)
+        
+        let task = URLSession.shared.dataTask(with: request) {
+            (data, response, error) in
+            
+            if let e = error{
+                print("끄엑", e)
+            }
+            
+            DispatchQueue.main.sync() {
+                
+                let responseDataJson:[String: Any] = JsonTool().serverResponeseToJson(data: data)
+                
+                if responseDataJson["success"] as! Bool {
+                    
+                    self.myInfo.accessToken = responseDataJson["accessToken"] as! String
+                    self.myInfo.refreshToken = responseDataJson["refreshToken"] as! String
+                    self.myInfo.id = id
+                    
+                    //
+                    //
+                    //                    /////
+                    //                    let url_for_request = URL(string: "http://139.150.64.36:9530/auth/login")
+                    //
+                    //                    let pw_encoded = pw.data(using: .utf8)?.base64EncodedString()
+                    //
+                    //                    let raw: [String : Any] = ["user_id": id, "user_pw":pw_encoded!]
+                    //
+                    //                    let formDataString = (raw.compactMap({ (key, value) -> String in return "\(key)=\(value)" }) as Array).joined(separator: "&")
+                    //
+                    //                    var request = URLRequest(url: url_for_request!)
+                    //                    request.httpMethod = "POST"
+                    //                    request.httpBody = formDataString.data(using: .utf8)
+                    //
+                    //
+                    //
+                    //
+                    //
+                    
+                    self.isSuccessLogin.toggle()
+                } else {
+                    self.showAlertLogin.toggle()
+                }
+                
+            }
+        }
+        
+        task.resume()
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-		View_login()
+        ViewLogin().environmentObject(MyInfo())
     }
 }
